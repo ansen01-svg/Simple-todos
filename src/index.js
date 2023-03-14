9149ba62b5634d2026eaeb144948422c2faa9bd5
ansen01-svg <ansenbeyc73@gmail.com>
2023-03-14 16:34:39 +0530

    finalised the app designs

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
+import { CssBaseline } from '@mui/material';
+import { ThemeProvider } from '@mui/system';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
+import theme from './theme/theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
